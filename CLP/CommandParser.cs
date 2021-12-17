9fa726426718481a966ddf2473e0eaec4af36a6b
Finite <remieluri@gmail.com>
2021-12-16 22:16:51 -0500

    Moved Extensions to it's own file

--- CLP/CommandParser.cs
+++ CLP/CommandParser.cs
@@ -224,24 +224,3 @@ public sealed class CommandParser
         }
     }
 }
-
-public static class Extentions
-{
-    public static bool IsInteger(this ArgumentType type)
-    {
-        switch (type)
-        {
-            case ArgumentType.Int8:
-            case ArgumentType.Int16:
-            case ArgumentType.Int32:
-            case ArgumentType.Int64:
-            case ArgumentType.Uint8:
-            case ArgumentType.Uint16:
-            case ArgumentType.Uint32:
-            case ArgumentType.Uint64:
-                return true;
-            default:
-                return false;
-        }
-    }
-}
---
+++ CLP/Extensions.cs
@@ -0,0 +1,22 @@
+﻿namespace CLP;
+
+public static class Extensions
+{
+    public static bool IsInteger(this ArgumentType type)
+    {
+        switch (type)
+        {
+            case ArgumentType.Int8:
+            case ArgumentType.Int16:
+            case ArgumentType.Int32:
+            case ArgumentType.Int64:
+            case ArgumentType.Uint8:
+            case ArgumentType.Uint16:
+            case ArgumentType.Uint32:
+            case ArgumentType.Uint64:
+                return true;
+            default:
+                return false;
+        }
+    }
+}
